d77229a9e3de5f36fa2410beeea2c50347dfbedc
0sugo <josugo26@gmail.com>
2023-08-01 18:03:45 +0300

    Add food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,39 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def show
+    @food = Food.find(params[:id])
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+
+    if @food.save
+      # redirect_to foods_path, notice: 'Food added succesfully'
+      flash[:notice] = 'Food added successfully!'
+      redirect_to foods_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+
+    redirect_to foods_path, notice: 'Food was successfully deleted.'
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
