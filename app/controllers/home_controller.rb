e88dd5c4410a36ab6f7614899380e71b82c16004
skabeo <spencerokyere95@gmail.com>
2023-08-01 02:54:34 +0000

    Add home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
-  def index; end
+  before_action :authenticate_user!, except: %i[index show]
+
+  def index
+    @user_name = if user_signed_in?
+                   extract_user_name(current_user.email)
+                 else
+                   'Guest'
+                 end
+  end
+
+  private
+
+  def extract_user_name(email)
+    email.split('@').first.capitalize
+  end
 end
