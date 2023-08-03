92b133784960019511ac8e87dac29eee41ccc6fe
skabeo <spencerokyere95@gmail.com>
2023-08-03 21:39:26 +0000

    Retrict authentication actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!
-
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @recipes = Recipe.all
   end
