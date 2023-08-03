94a3f9760a43cf7c6750a9dbb9520fa1e7abff9e
0sugo <josugo26@gmail.com>
2023-08-04 00:04:32 +0300

    Fix rubocop

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Food, type: :model do
       it 'validates length of name' do
         food = Food.new(name: 'a' * 101)
         expect(food).not_to be_valid
-        expect(food.errors[:name]).to include("is too long (maximum is 100 characters)")
+        expect(food.errors[:name]).to include('is too long (maximum is 100 characters)')
       end
 
       it 'validates presence of measurement_unit' do
@@ -31,7 +31,7 @@ RSpec.describe Food, type: :model do
       it 'validates length of measurement_unit' do
         food = Food.new(measurement_unit: 'a' * 51)
         expect(food).not_to be_valid
-        expect(food.errors[:measurement_unit]).to include("is too long (maximum is 50 characters)")
+        expect(food.errors[:measurement_unit]).to include('is too long (maximum is 50 characters)')
       end
 
       it 'validates presence of price' do
@@ -43,13 +43,13 @@ RSpec.describe Food, type: :model do
       it 'validates numericality of price' do
         food = Food.new(price: 'abc')
         expect(food).not_to be_valid
-        expect(food.errors[:price]).to include("is not a number")
+        expect(food.errors[:price]).to include('is not a number')
       end
 
       it 'validates price is greater than or equal to 0' do
         food = Food.new(price: -1)
         expect(food).not_to be_valid
-        expect(food.errors[:price]).to include("must be greater than or equal to 0")
+        expect(food.errors[:price]).to include('must be greater than or equal to 0')
       end
 
       it 'validates presence of quantity' do
@@ -61,13 +61,13 @@ RSpec.describe Food, type: :model do
       it 'validates numericality of quantity' do
         food = Food.new(quantity: 'abc')
         expect(food).not_to be_valid
-        expect(food.errors[:quantity]).to include("is not a number")
+        expect(food.errors[:quantity]).to include('is not a number')
       end
 
       it 'validates quantity is greater than or equal to 0' do
         food = Food.new(quantity: -1)
         expect(food).not_to be_valid
-        expect(food.errors[:quantity]).to include("must be greater than or equal to 0")
+        expect(food.errors[:quantity]).to include('must be greater than or equal to 0')
       end
     end
   end
