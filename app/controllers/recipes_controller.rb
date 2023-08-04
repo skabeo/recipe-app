869242b06250c91b10d373ee09bfc08af1044e29
skabeo <spencerokyere95@gmail.com>
2023-08-04 00:53:26 +0000

    Modify recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :authenticate_user!, except: %i[index show]
+
   def index
     @recipes = Recipe.all
   end
